ed95441ad53858c494afc4eb3079618e366c4bed
Manav Vora <manavvora@wirelessprv-10-193-137-69.near.illinois.edu>
2024-08-19 12:15:45 -0500

    updates - code refactoring

--- src/comtraq-mpc/comtraq-mpc_test.py
+++ src/comtraq-mpc/comtraq-mpc_test.py
@@ -1,151 +1,97 @@
 import numpy as np
-import seaborn as sns
 from matplotlib import pyplot as plt
-from stable_baselines3 import DQN
-from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.dqn.policies import MlpPolicy
-
 from model import USV
+from stable_baselines3 import DQN
+import seaborn as sns
+import config
 
-final_path = np.genfromtxt("data/turtlebot_positions.csv", delimiter=",", skip_header=1)
-final_path = final_path[::10]
-final_path[:, :2] *= 15
+final_path = np.load(config.final_path)
+final_path*=15
+
+
 initial_positions = [(final_path[0][0], final_path[0][1], final_path[0][2])]
-final_path = final_path[:, :2]
-# final_path = final_path*15
+final_path = final_path[:,:2]
 
 env = USV(
     v=0,
     dt=0.2,
     path_index=0,
     goal=final_path[-1],
-    budget=10,
+    budget=20,
     initial_positions=initial_positions,
     final_paths=[final_path],
 )
 
-model = DQN.load(
-    "trained_models/dqn_communication_optimization_epsfrac07_steps200k_turtlebot_path_niket"
-)
+model = DQN.load(config.model_path)
 
 obs, _ = env.reset()
 done = False
 total_rewards = 0
 ep_var = []
-# print(env.path_index)
 errors = []
-# print(errors)
+traj_errors = [np.linalg.norm([env.car.x_true, env.car.y_true] - final_path[env.path_index])]
 print("-------")
 communicate_indices = []
 variances = []
 car_positions = [[env.car.x_true, env.car.y_true]]
-traj_errors = [
-    np.linalg.norm([env.car.x_true, env.car.y_true] - final_path[env.path_index])
-]
+observations = [np.array([env.car.x, env.car.y, env.car.v, env.car.psi, env.available_budget], dtype=np.float32)]
+waypoint_deviations = 0
 while not done:
-    # if env.path_index <= 100:
-    #     action = 0
-    # else:
-    action, _states = model.predict(
-        obs[:5], deterministic=True
-    )  # Use the model to predict the action
-    # action = 1
-    # print(f"action: {action}")
+    if np.linalg.norm([env.car.x, env.car.y] - final_path[env.path_index]) > 0.2*15:
+        waypoint_deviations += 1
+    if env.path_index <= 20:
+        action = 0
+    else:
+        
+        action, _states = model.predict(
+            obs[:5], deterministic=True
+        )  # Use the model to predict the action
     if action == 1:
         communicate_indices.append(env.path_index)
 
-    # print(f"action: {action}")
-    # ep_var.append(np.linalg.norm(env.car.pf_var))
+    traj_errors.append(np.linalg.norm([env.car.x_true, env.car.y_true] - final_path[env.path_index]))
     errors.append(
         np.linalg.norm(
-            np.array([env.car.x, env.car.y])
-            - np.array([env.car.x_true, env.car.y_true])
-        )
-    )
+            np.array([env.car.x, env.car.y]) - np.array([env.car.x_true, env.car.y_true])
+        ))
     variances.append(np.linalg.norm(env.car.pf_var))
-    traj_errors.append(
-        np.linalg.norm([env.car.x_true, env.car.y_true] - final_path[env.path_index])
-    )
     obs, rewards, terminated, truncated, info = env.step_test(
         action
     )  # Take the action in the environment
-    # print(f"rewards: {rewards}")
-    # print(f"info: {info}")
+    observations.append(obs[:5])
     car_positions.append([env.car.x_true, env.car.y_true])
     total_rewards += rewards
     done = truncated or terminated
-    # print(f"obs: {obs}")
 
-print(len(communicate_indices))
-plt.subplot(2, 1, 2)
-plt.plot(errors)
-plt.scatter(communicate_indices, [errors[i] for i in communicate_indices], c="r")
-plt.title("Error")
-plt.subplot(2, 1, 1)
-plt.plot(variances)
-plt.scatter(communicate_indices, [variances[i] for i in communicate_indices], c="r")
-plt.title("Variance")
-plt.show()
+print(f'Total active localizations performed: {len(communicate_indices)}')
 
-
-waypoints_followed = 0
-
-for i in range(len(traj_errors)):
-    if traj_errors[i] < 7:
-        waypoints_followed += 1
-print(waypoints_followed)
-
-print("mean absolute error: ", np.mean(traj_errors))
-
-plt.plot(final_path[:, 0], final_path[:, 1], "r", label="Reference Path")
-plt.plot(
-    np.array(car_positions)[:, 0], np.array(car_positions)[:, 1], "b", label="MPC Path"
-)
+sns.set_theme(style="whitegrid")
+plt.figure(figsize=(10, 6))
+plt.plot(traj_errors, label="Error", linestyle='-', marker='o', markersize=4, linewidth=2, color='#BDB2FF')
+plt.scatter(communicate_indices, [traj_errors[i] for i in communicate_indices], c='r', label="Communicate points", s=50, edgecolor='black', zorder=5)
+plt.title("Variation of Error", fontsize=16, fontweight='bold')
+plt.xlabel("Time step", fontsize=14)
+plt.ylabel("Error", fontsize=14)
 plt.legend()
+sns.despine()
 plt.show()
-
-print(np.array(car_positions))
 
 
 sns.set_theme(style="whitegrid")
-
 plt.figure(figsize=(10, 6))
-
-plt.plot(
-    final_path[:, 0] / 15,
-    final_path[:, 1] / 15,
-    "r-",
-    linewidth=7,
-    label="Reference Path",
-)
-
-plt.plot(
-    np.array(car_positions)[:, 0] / 15,
-    np.array(car_positions)[:, 1] / 15,
-    "b-",
-    linewidth=7,
-    label="ComTraQ-MPC Path",
-)
-
-plt.scatter(
-    np.array(car_positions)[communicate_indices, 0] / 15,
-    np.array(car_positions)[communicate_indices, 1] / 15,
-    c="cyan",
-    s=200,
-    edgecolor="black",
-    label="Communicate Points",
-    zorder=5,
-)
-
-plt.legend(fontsize=12)
-
-plt.title("Path Comparison", fontsize=16, fontweight="bold")
-plt.xlabel("Position X", fontsize=14)
-plt.ylabel("Position Y", fontsize=14)
-
+plt.plot(errors, label="Error", linestyle='-', marker='o', markersize=4, linewidth=2, color='#BDB2FF')
+plt.scatter(communicate_indices, [errors[i] for i in communicate_indices], c='r', label="Communicate points", s=50, edgecolor='black', zorder=5)
+plt.title("Variation of Error", fontsize=16, fontweight='bold')
+plt.xlabel("Time step", fontsize=14)
+plt.ylabel("Error", fontsize=14)
+plt.legend()
 sns.despine()
-plt.grid(False)
 plt.show()
 
-np.linalg.norm(final_path[0] - final_path[-1])
+plt.plot(final_path[:, 0], final_path[:, 1], "r", label="Reference Path")
+plt.plot(np.array(car_positions)[:, 0], np.array(car_positions)[:, 1], "b", label="MPC Path")
+plt.scatter(final_path[communicate_indices, 0], final_path[communicate_indices, 1], c='r', label="Communicate points", s=50, edgecolor='black', zorder=5)
+plt.legend()
+plt.show()
+
+
--- src/comtraq-mpc/comtraq-mpc_train.py
+++ src/comtraq-mpc/comtraq-mpc_train.py
@@ -1,62 +1,44 @@
-import argparse
-
-import numpy as np
-
-# from pathplanning import ParkPathPlanning, PathPlanning, interpolate_path
-from stable_baselines3 import DQN
 from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3 import DQN
 from stable_baselines3.dqn.policies import MlpPolicy
+from stable_baselines3.common.env_util import make_vec_env
 
-from control_2 import Car_Dynamics, MPC_Controller, ParticleFilter
-from environment import Environment, Parking1
-
-# from stable_baselines3.common.envs import DummyVecEnv
 from model import USV
 
-# from stable_baselines3.common.vec_env import DummyVecEnv
+
+import config
 
 
-final_path = np.genfromtxt(
-    "dense_lawnmower_pattern_200_waypoints.csv", delimiter=",", skip_header=1
-)
-# final_path = final_path[::10]
-# final_path[:,:2]*=15
 
 
-initial_positions = [(final_path[0][0], final_path[0][1], 90.0)]
-# print(initial_positions)
-final_path = final_path[:, :2]
+final_path = config.final_path
+final_path*=15 # scaling the path -- can be removed if not necessary
+
+
+initial_positions = [(final_path[0][0], final_path[0][1], final_path[0][2])]
+final_path = final_path[:,:2]
 
 
 env = USV(
-    v=0,
-    dt=0.2,
-    path_index=0,
-    goal=final_path[-1],
-    budget=50,
+    v=config.v,
+    dt=config.dt,
+    path_index=config.path_index,
+    goal=config.goal,
+    budget=config.budget,
     initial_positions=initial_positions,
     final_paths=[final_path],
 )
-check_env(env)  # Optional: Check if the environment follows Gym API
-# env.set_initial_state(args.x_start, args.y_start, args.psi_start)
-# vec_env = DummyVecEnv([lambda: env])
-# Parameters to modify
-buffer_size = 10000  # Size of the replay buffer
-learning_rate = 1e-3  # Learning rate
-batch_size = 128  # Size of the batch for learning
-gamma = 0.9  # Discount factor
-exploration_fraction = (
-    0.7  # Fraction of entire training period over which the exploration rate is reduced
-)
-exploration_final_eps = 0.02  # Final value of random action probability
-target_update_interval = (
-    1000  # Number of steps after which the target network is updated
-)
-train_freq = (1, "episode")  # Update the model every 'train_freq' steps
-gradient_steps = -1  # Number of gradient steps to take after each environment step
-log_dir = "tmp/dqn/"  # Where to log the model
-# Initialize the model with custom parameters
+check_env(env)  
+buffer_size = config.buffer_size  
+learning_rate = config.learning_rate
+batch_size = config.batch_size
+gamma = config.gamma
+exploration_fraction = config.exploration_fraction
+exploration_final_eps = config.exploration_final_eps
+target_update_interval = config.target_update_interval
+train_freq = config.train_freq
+gradient_steps = config.gradient_steps
+log_dir = config.log_dir
 model = DQN(
     MlpPolicy,
     env,
@@ -73,10 +55,7 @@ model = DQN(
     tensorboard_log=log_dir,
 )
 
-# Train the model
-model.learn(total_timesteps=1000000)
 
-# Save the model
-model.save(
-    "dqn_communication_optimization_epsfrac07_steps1M_50bud_200_waypoints_lawnmower_path"
-)
+model.learn(total_timesteps=config.total_timesteps)
+
+model.save(config.model_path)
